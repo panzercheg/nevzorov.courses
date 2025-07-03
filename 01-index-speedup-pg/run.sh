664a6dcfe4e9b24af0838d9053f5c8f7a090caad
Mikhail Masyagin <masyagin1998@yandex.ru>
2025-07-03 09:21:16 +0000

    FIX: renaming

--- 01-index-speedup-pg/run.sh
+++ 01-index-speedup-pg/run.sh
@@ -3,5 +3,5 @@ set -e
 pip install -r requirements.txt
 docker compose up -d
 sleep 5
-python speedtest.py
+python index_speedup.py
 docker compose down 
--- 04-fts-es/run.sh
+++ 04-fts-es/run.sh
@@ -3,5 +3,5 @@ set -e
 pip install -r requirements.txt
 docker compose up -d
 sleep 5
-python speedtest.py
+python fts.py
 docker compose down 
